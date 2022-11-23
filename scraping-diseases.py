0abeeb18427bc64c54bde5f1702c8d4b630f2009
Petr Musil <petr0musil@gmail.com>
2022-11-23 12:45:42 +0100

    Writing data into a file relocated to the try block. Comment added.

--- scraping-diseases.py
+++ scraping-diseases.py
@@ -6,10 +6,11 @@ from csv import reader, writer
 from time import sleep
 
 # the source file path of the codes of diseases
-file_path_source = pathlib.Path("./data/annot_KEGG_manualdownl_terms.csv")
+file_path_source = pathlib.Path("./data/annot_KEGG_manualdownl_terms_test.csv")
 
 # prevents missing source file and os errors
 try:
+    # requests data from web using the file path and saves into the result_list
     with open(file_path_source, "r") as file:
         csv_reader = reader(file)
         next(csv_reader)
@@ -21,12 +22,13 @@ try:
             result_list.append([disease_code, soup.find(
                 text="Description").findNext("div").text])
             sleep(8)
+
+    # writes the downloaded data into a file
+    with open("./data/list_of_diseases_and_codes.csv", "w") as file:
+        csv_writer = writer(file)
+        for disease in result_list:
+            csv_writer.writerow([disease[0][0], disease[1]])
+        print(f"Done. Downloaded {len(result_list)} of diseases.")
+
 except OSError as error:
     logging.error(f"Writing to file {file_path_source} failed due to: {error}")
-
-# writes the downloaded data into a file
-with open("./data/list_of_diseases_and_codes.csv", "w") as file:
-    csv_writer = writer(file)
-    for disease in result_list:
-        csv_writer.writerow([disease[0][0], disease[1]])
-    print(f"Done. Downloaded {len(result_list)} of diseases.")
